4153e3f47f1785cf424b6a29cbc1e7543ea31e90
Dev-Varsha <varshaelango8986@gmail.com>
2025-05-07 18:04:18 +0530

    push for backend isuue

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
+#!/usr/bin/env sh
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
 python manage.py migrate
-if [[ $CREATE_SUPERUSER ]];
-then
-    python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+
+if [ "$CREATE_SUPERUSER" = "true" ]; then
+  python manage.py createsuperuser \
+    --no-input \
+    --username "$DJANGO_SUPERUSER_USERNAME" \
+    --email "$DJANGO_SUPERUSER_EMAIL"
 fi
